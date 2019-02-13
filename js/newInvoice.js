aec1c68dc068974a0c3db014c16ec7aab673d6ec
ejaworska <ewel.jaworska@gmail.com>
2019-02-13 11:15:12 +0100

    Corect css and handle logo click

--- js/newInvoice.js
+++ js/newInvoice.js
@@ -10,6 +10,29 @@ window.onload = function () {
 
     let addNewItem = document.getElementById("add-item");
     addNewItem.addEventListener("click", handleAddNewItemClick);
+
+    var logos = document.getElementsByClassName("logo-to-get");
+    addLogoClickListener(logos);
+    document.getElementById("upload-logo").addEventListener("click", showGallery);
+}
+
+function addLogoClickListener(logos) {
+    for (var i = 0; i < logos.length; i++) {
+        var logo = logos[i];
+        logo.addEventListener("click", getSrc);
+    }
+}
+
+function getSrc() {
+    document.getElementById("logo-gallery").style.display = "none";
+    document.getElementById("cover").style.display = "none";
+    return this.getAttribute("src");
+}
+
+
+function showGallery() {
+    document.getElementById("logo-gallery").style.display = "flex";
+    document.getElementById("cover").style.display = "block";
 }
 
 function calculateTotal() {
