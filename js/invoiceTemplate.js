ccb37f40630b842233e6051a4e2004eeeadbb8df
Kamil <kamil.m.olsza@gmail.com>
2019-02-14 14:42:10 +0100

    Print invoice feature ready

--- js/invoiceTemplate.js
+++ js/invoiceTemplate.js
@@ -19,16 +19,42 @@ window.onload = function () {
     today = mm + '/' + dd + '/' + yyyy;
 
     let invoiceNum = location.hash.split("#")[1];
-    document.getElementById("invoice-number").innerHTML = "Invoice #: " + invoiceNum + "<br>Created: " + today + "<br>Due: 14 days";
-    
+
     let invoices = JSON.parse(localStorage.getItem("invoices"));
-    // debugger 
+    
+    let invoiceDate = invoices[invoiceNum].invoiceDate;
+    document.getElementById("invoice-number").innerHTML = "Invoice #: " + invoiceNum + "<br>Created: " + invoiceDate + "<br>Due: 14 days";
+    
+    // debugger   
+
+    let customerName = invoices[invoiceNum].billTo;
+    document.getElementById("customer").innerHTML = customerName;
+
+    let dealerName = invoices[invoiceNum].from;
+    document.getElementById("dealer").innerHTML = dealerName;
+
     // for (let i=0;i<invoices.length; i++){
     //     let name = invoices[i].billTo;
-    // }    
-    let name = invoices[0].billTo;
+    // } 
 
-    document.getElementById("customer").innerHTML = name;
+    let descItem = invoices[invoiceNum].items[0].description;
+    document.getElementById("descItem").innerHTML = descItem;
+
+    let itemAmount = invoices[invoiceNum].items[0].amount;
+    document.getElementById("itemAmount").innerHTML = itemAmount;
+
+    let itemPrice = invoices[invoiceNum].items[0].price;
+    document.getElementById("itemPrice").innerHTML = itemPrice;
+
+    let itemTax = invoices[invoiceNum].items[0].tax;
+    document.getElementById("itemTax").innerHTML = itemTax;
+
+    let totalPrice = invoices[invoiceNum].items[0].price;
+    document.getElementById("totalPrice").innerHTML = "<strong>Total: $"+totalPrice+"<strong>";
+
+    let conditions = invoices[invoiceNum].termsAndConditions;
+    document.getElementById("conditions").innerHTML = "<strong>Additional terms and conditions:</strong><br>"+conditions;
+
     // printPage();
 }
 
