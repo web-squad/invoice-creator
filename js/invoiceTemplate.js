682bdd5aca47f5d1b0cf1ab2edb68e6e8ce35f8b
Kamil <kamil.m.olsza@gmail.com>
2019-02-14 09:58:53 +0100

    Customer adress fix

--- js/invoiceTemplate.js
+++ js/invoiceTemplate.js
@@ -22,10 +22,11 @@ window.onload = function () {
     document.getElementById("invoice-number").innerHTML = "Invoice #: " + invoiceNum + "<br>Created: " + today + "<br>Due: 14 days";
     
     let invoices = JSON.parse(localStorage.getItem("invoices"));
+    // debugger 
     // for (let i=0;i<invoices.length; i++){
     //     let name = invoices[i].billTo;
     // }
-    let name = invoices[1].billTo;
+    let name = invoices[0].billTo;
 
     document.getElementById("customer").innerHTML = name;
     // printPage();
